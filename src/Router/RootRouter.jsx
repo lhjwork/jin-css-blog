c33ad57de2e6797a42a8ae3bc3080f93eb5ac853
lhjwork <jin.come.up.business@gmail.com>
2023-10-06 14:26:43 +0900

    react-router-dom

--- src/Router/RootRouter.jsx
+++ src/Router/RootRouter.jsx
@@ -3,12 +3,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Root from "../pages/Root";
 import NotFound from "../pages/error/NotFound";
+import TestPage from "../pages/test/TestPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <NotFound />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: "test", element: <TestPage /> },
+    ],
   },
 ]);
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import styles from "./navbar.module.css";
+import { CgMenuGridR } from "react-icons/cg";
+import { SiVisualbasic } from "react-icons/si";
+import { MdAnimation } from "react-icons/md";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <aside>
+      <section className="side-bar__icon-box">
+        <section className="side-bar__icon-1">
+          <CgMenuGridR />
+        </section>
+      </section>
+      <ul>
+        <li>
+          <SiVisualbasic />
+        </li>
+        <ul>
+          <li>
+            <Link to={`test`}>test</Link>
+          </li>
+        </ul>
+        <li>
+          <MdAnimation />
+        </li>
+        <ul>
+          <li>
+            <Link to={`/`}>home</Link>
+          </li>
+        </ul>
+      </ul>
+    </aside>
+  );
 };
 
 export default Navbar;
