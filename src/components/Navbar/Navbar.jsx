8649492e5a2d650e5e2c1661b46bc04df91d33c4
lhjwork <jin.come.up.business@gmail.com>
2023-10-06 14:48:07 +0900

    NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,34 +3,29 @@ import styles from "./navbar.module.css";
 import { CgMenuGridR } from "react-icons/cg";
 import { SiVisualbasic } from "react-icons/si";
 import { MdAnimation } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <aside>
-      <section className="side-bar__icon-box">
-        <section className="side-bar__icon-1">
-          <CgMenuGridR />
-        </section>
-      </section>
-      <ul>
-        <li>
-          <SiVisualbasic />
-        </li>
-        <ul>
-          <li>
-            <Link to={`test`}>test</Link>
-          </li>
-        </ul>
-        <li>
-          <MdAnimation />
-        </li>
-        <ul>
-          <li>
-            <Link to={`/`}>home</Link>
-          </li>
-        </ul>
-      </ul>
-    </aside>
+    <>
+      <div id="sidebar">
+        <nav>
+          <ul>
+            <li>
+              <CgMenuGridR />
+              <NavLink
+                to={`test`}
+                cclassName={({ isActive, isPending }) =>
+                  isActive ? "active" : isPending ? "pending" : ""
+                }
+              >
+                Test Navigate
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 };
 
